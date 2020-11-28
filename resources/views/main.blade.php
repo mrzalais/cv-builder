96ee7d67569ec718a2e142b139dc44a3194d2f01
mrzalais <lejnieksedijs@gmail.com>
2020-11-28 13:25:09 +0200

    init/installl framework

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+        <h2>CV būvētājs</h2>
+
+        <button class="btn btn-primary">Sākt</button>
+
+    </div>
+
+@endsection
